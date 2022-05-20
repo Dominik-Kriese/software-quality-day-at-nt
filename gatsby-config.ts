1a660f11817755f869562fb3ebd2de60c5d191d7
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2022-05-20 11:55:24 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Software Quality Day @ Novatec`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
+
+export default config;

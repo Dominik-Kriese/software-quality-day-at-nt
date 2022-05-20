afc4044a26c8c08a10c719f40cad75338e57dbcf
Dominik Kriese <dominik.kriese@novatec-gmbh.de>
2022-05-20 15:49:07 +0200

    Updates

---
+++ cb1608f2-95940c059e22341cb616.js
@@ -0,0 +1,3 @@
+/*! For license information please see cb1608f2-95940c059e22341cb616.js.LICENSE.txt */
+"use strict";(self.webpackChunksoftware_quality_day_novatec=self.webpackChunksoftware_quality_day_novatec||[]).push([[523],{8014:function(a,e,c){c.d(e,{IyC:function(){return n}});var n={prefix:"fas",iconName:"video",icon:[576,512,["video-camera"],"f03d","M384 112v288c0 26.51-21.49 48-48 48h-288c-26.51 0-48-21.49-48-48v-288c0-26.51 21.49-48 48-48h288C362.5 64 384 85.49 384 112zM576 127.5v256.9c0 25.5-29.17 40.39-50.39 25.79L416 334.7V177.3l109.6-75.56C546.9 87.13 576 102.1 576 127.5z"]}}}]);
+//# sourceMappingURL=cb1608f2-95940c059e22341cb616.js.map
